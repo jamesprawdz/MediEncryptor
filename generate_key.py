e09b934915abd42e62722f98f48e9a5dce33d0fb
JamesPrawdzik <sebaprawdzik@gmail.com>
2023-12-29 12:13:36 -0500

    upgraded from AES-128 key(fernet) to AES-256

--- generate_key.py
+++ generate_key.py
@@ -1,11 +1,12 @@
-from cryptography.fernet import Fernet
+import os
 
 
 def generate_encryption_key():
-    key = Fernet.generate_key()
+    # 32 bytes = 256 bits
+    key = os.urandom(32)  # Generate a 256-bit random key
     return key
 
 
 if __name__ == "__main__":
     key = generate_encryption_key()
-    print("Generated Encryption Key:", key.decode())
+    print("Generated Encryption Key:", key.hex())
